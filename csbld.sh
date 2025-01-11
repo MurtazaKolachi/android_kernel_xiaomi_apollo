340d5106967e422820c9ab199b289dc70de3dee7
Aliza <alirezanajmabadi79@gmail.com>
2025-01-11 07:20:35 +0000

    adding susfs

--- csbld.sh
+++ csbld.sh
@@ -4,7 +4,6 @@ export SUBARCH=ARM64
 export KBUILD_BUILD_USER="aliza"
 export KBUILD_BUILD_HOST="Github"
 rm -rf out
-git submodule init && git submodule update
 export LD_LIBRARY_PATH="~/clang/lib64:$LD_LIBRARY_PATH}"
 make O=out vendor/alioth_defconfig PATH="/workspaces/clang/bin:$PATH"
 make O=out CC=clang -j$(nproc --all) PATH="/workspaces/clang/bin:$PATH" CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1
